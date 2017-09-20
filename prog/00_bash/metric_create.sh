a510a36c4f54a2acdf44ba6b8005dcabe076c74d
rmp15 <r.parks15@imperial.ac.uk>
2017-09-20 12:00:56 +0200

    added stuff

--- prog/00_bash/metric_create.sh
+++ prog/00_bash/metric_create.sh
@@ -19,6 +19,7 @@ for var in "${vars[@]}"; do
 for model in "${models[@]}"; do
 
 # create file architecture to store files
+# BE ULTRA CAREFUL WITH THIS LINE IT CAN DESTROY WORLDS
 dir="//data/climate/net_cdf/" ; dir+="$var/" ; dir+="$rcp/" ; dir+="$model/" ; dir+="raw/" ;
 mkdir -p "$dir"
 
